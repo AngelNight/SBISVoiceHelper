ebdc56d02fab615ab5cdfab560af1f6556d6430c
pa.solodov <pa.solodov@tensor.ru>
2015-11-03 10:11:59 +0300

    добавлен manifest.json, иконка и стартовый файл для теста подключаемости плагина

---
+++ app.js
@@ -0,0 +1,4 @@
+//todo: убрать, было сделано для теста
+var birdStyle = document.getElementById('logoPtica').style;
+birdStyle.backgroundImage = "none";
+birdStyle.backgroundColor = "rgb(" + parseInt(Math.random()*256) + ',' + parseInt(Math.random()*256) + ',' + parseInt(Math.random()*256) + ")";
