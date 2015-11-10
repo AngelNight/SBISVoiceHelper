3d22639d3756de74d2a132ef6f906371d0350fe2
pa.solodov <pa.solodov@tensor.ru>
2015-11-10 11:15:34 +0300

    стилизована иконка поправлен обработчик по клику на кнопку поправлен стиль кнопки

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-//todo: убрать, было сделано для теста
 var bird = document.getElementById('logoPtica');
 if(bird){
    bird.className += 'bird icon-32 icon-Microphone icon-primary';
    bird.title = 'SBIS Voice Helper';
-   bird.click = function(){
+   bird.addEventListener("click", function(){
       alert('SBIS Voice Helper');
-   };
+   });
 }
