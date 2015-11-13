6e13184ff9bac1acbb782b3201c2ca862d19d57c
miropeace <miropeace@gmail.com>
2015-11-13 19:50:52 +0300

    Удалены скрипты Yandex Speach Kit Добавлен TTS от Google Удалён код для совместимости с IE 8

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-var bird = document.getElementById('logoPtica');
+function $(id){
+	return document.getElementById(id);
+}
+
+function Say(utterance, options, callback) {
+  if(!options) options = {voiceName:"Google русский"};
+  console.log('Говорю: ' + utterance);
+  if (callback)
+	chrome.runtime.sendMessage({utterance,options,callback}, function() {});
+  else
+    chrome.runtime.sendMessage({utterance,options}, function() {});
+ 
+}
+
+var bird = $('logoPtica');
+
 if(bird){
    bird.className += 'bird icon-32 icon-Microphone icon-primary';
    bird.title = 'SBIS Voice Helper';
 
    // Google Web Speech API
    var rec = new webkitSpeechRecognition(),
-         sHadler = new SpeechHandler();
+       sHadler = new SpeechHandler();
+
    rec.continuous = true;
    rec.interimResults = true;
-   rec.lang = 'ru';
+   rec.lang='ru';
    rec.onstart = function () {
+	  //chrome.tts.speak("Say IT");
+	  Say("Привет, хозяин");
       rec.isRunning = true;
    };
    rec.onend = function () {
+	  Say("До скорой встречи, хозяин");
       rec.isRunning = false;
    };
    rec.onresult = function (event) {
